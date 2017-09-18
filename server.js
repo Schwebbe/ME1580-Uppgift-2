b83253dbbff01cb0c61dc6702a7bed050f4bd8cf
Schwebbe <sebbetheme@hotmail.com@hotmail.com>
2017-09-18 17:04:41 +0200

    Another update

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post("/images", function (req, res) {
     database.post(req.body).then(function (result) {
         //Detta kommando gör så att när post requesten skickas itll servern, so respondar den med
         //att refresha sidan via detta direktiv som anges
-        res.sendFile((path.join(__dirname, '/public/index.html')));
+        res.sendFile((path.join(__dirname, '/public/admin.html')));
     });
 });
 //Här tas data bort från customers och returnerar resultaten från databasen 
